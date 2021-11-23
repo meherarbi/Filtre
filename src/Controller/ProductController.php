149428467730ea87cf11f908bb930d649640854e
meherarbi <meher.arbi@gmail.com>
2021-11-23 18:19:13 +0100

    preparer les champs de filtre

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,16 +6,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ProductRepository;
+use App\Data\SearchData;
+use App\Form\SearchForm ;
 class ProductController extends AbstractController
 {
     /**
      * @Route("/", name="product")
      */
     public function index(ProductRepository $productRepository): Response
+
     {
+        $data = new SearchData();
+        $form = $this->createForm(SearchForm::class, $data);
         $products = $productRepository->findSearch();
         return $this->render('product/index.html.twig', [
             'products' => $products,
+            'form' => $form->createView(),
         ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,6 +6,7 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use App\Entity\Product;
+use App\Entity\Category;
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
@@ -13,21 +14,30 @@ class AppFixtures extends Fixture
         $faker= Factory::create('fr_FR');
          $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker)); 
         $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+        $faker->addProvider(new \Bezhanov\Faker\Provider\Space($faker));
         
        
           $product = [];
-        for ($i = 0; $i < 50; $i++) {
-             $product[$i] = new Product();
-            $product[$i]->setName($faker->productName);
-            $product[$i]->setPrice($faker->numberBetween($min = 50, $max = 6000));
-            $product[$i]->setDescription($faker->text(100));
-            $product[$i]->setImage($faker->imageUrl(500, 240,true));
-            $product[$i]->setPromo('10%? 1 : 0');
+          for ($i = 0; $i < 5; $i++) {
+              $category = new Category();
+              $category->setName($faker->moon());
+              $manager->persist($category);
+          }
+              for ($i = 0; $i < 21; $i++) {
+                  $product[$i] = new Product();
+                  $product[$i]->setName($faker->productName);
+                  $product[$i]->setPrice($faker->numberBetween($min = 50, $max = 6000));
+                  $product[$i]->setDescription($faker->text(100));
+                  $product[$i]->setImage($faker->imageUrl(500, 240, true));
+                  $product[$i]->setPromo('10%? 1 : 0');
         
         
-            $manager->persist($product[$i]);
+                  $manager->persist($product[$i]);
 
-            $manager->flush();
-        }
+                  
+              }
+              
+          
+          $manager->flush();
     }
 }
