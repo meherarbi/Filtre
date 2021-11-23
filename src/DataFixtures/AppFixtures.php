0ecb8fb9c234160d16b68b39397e36720382da10
meherarbi <meher.arbi@gmail.com>
2021-11-23 11:59:31 +0100

    design

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -11,8 +11,8 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $faker= Factory::create('fr_FR');
-        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-        
+         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker)); 
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
         
        
           $product = [];
@@ -21,7 +21,7 @@ class AppFixtures extends Fixture
             $product[$i]->setName($faker->productName);
             $product[$i]->setPrice($faker->numberBetween($min = 50, $max = 6000));
             $product[$i]->setDescription($faker->text(100));
-            $product[$i]->setImage($faker->imageUrl(400, 240));
+            $product[$i]->setImage($faker->imageUrl(500, 240,true));
             $product[$i]->setPromo('10%? 1 : 0');
         
         
